98aa2bec70906b57cca1dd0828947d4155daa281
JiwanChaudhary <jeewanchaudhary6@gmail.com>
2024-10-28 20:31:44 +0545

    fix: build error

--- src/app/(pages)/shop/page.tsx
+++ src/app/(pages)/shop/page.tsx
@@ -1,10 +1,13 @@
-import React from 'react'
+import React, { Suspense } from 'react'
 import VehicleParts from '@/components/home/vehicle/vehicle-parts'
+import { LoadingSpinner } from '@/components/ui/loading-spinner'
 
 const Page = () => {
   return (
     <section className='container my-2 md:my-4'>
-      <VehicleParts />
+      <Suspense fallback={<LoadingSpinner />}>
+        <VehicleParts />
+      </Suspense>
     </section>
   )
 }
