dd0e0be1703868d857a4bb51dc095aeb99e89a9e
JiwanChaudhary <jeewanchaudhary6@gmail.com>
2024-11-22 17:20:50 +0545

    fix: suv callout lines for small devices

--- src/components/home/vehicle/vehicle-type/suv/index.tsx
+++ src/components/home/vehicle/vehicle-type/suv/index.tsx
@@ -9,7 +9,7 @@ const Suv = () => {
       <Image
         src={suv}
         alt='vehicle'
-        className='max-h-[400px] max-w-[190px] object-cover'
+        className='max-h-[300px] max-w-[140px] object-cover md:max-h-[400px] md:max-w-[190px]'
       />
       <SuvCalloutLines />
     </>
