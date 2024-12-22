c949d68edeaca3ea9fa5ae73a31a13a9a5f8dfa9
JiwanChaudhary <jeewanchaudhary6@gmail.com>
2024-12-22 22:01:23 +0545

    feat: mobile service

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -4,6 +4,7 @@ import React from 'react'
 import TrustFactors from '@/components/home/trust-factors'
 import StatsSection from '@/components/home/stats'
 import ImageCarousel from '@/components/home/image-carousel'
+import MobileService from '@/components/home/mobile-service'
 
 const HomePageSection = ({ children }: { children: React.ReactNode }) => {
   return <section className='container my-2 md:my-4'>{children}</section>
@@ -14,6 +15,9 @@ const HomePage = () => {
     <main>
       <HomePageSection>
         <TrustFactors />
+      </HomePageSection>
+      <HomePageSection>
+        <MobileService />
       </HomePageSection>
       <HomePageSection>
         <ImageCarousel />
