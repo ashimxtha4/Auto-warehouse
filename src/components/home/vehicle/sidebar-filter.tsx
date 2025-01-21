807a3427c9f21688767e634cb19dc08f156f7d8d
JiwanChaudhary <jeewanchaudhary6@gmail.com>
2025-01-21 23:50:01 +0545

    chore: filter products

--- src/components/home/vehicle/sidebar-filter.tsx
+++ src/components/home/vehicle/sidebar-filter.tsx
@@ -4,13 +4,6 @@ import { LoadingSpinner } from '@/components/ui/loading-spinner'
 import { useSearchVehicles } from '@/hooks/search-vehicle.hooks'
 import { useVehicleParts } from '@/hooks/vehicle-parts.hook'
 import { cn } from '@/lib/utils'
-import { CiMenuBurger } from "react-icons/ci";
-import {
-  Popover,
-  PopoverContent,
-  PopoverTrigger,
-} from "@/components/ui/popover"
-import { Button } from '@/components/ui/button'
 
 interface SidebarContentProps {
   sidebarData: {
@@ -21,7 +14,7 @@ interface SidebarContentProps {
   specificPart: string | null | undefined
 }
 
-function SidebarContent({ handleSearchFilter, sidebarData, sidebarDataPending, specificPart }: SidebarContentProps) {
+export function SidebarContent({ handleSearchFilter, sidebarData, sidebarDataPending, specificPart }: SidebarContentProps) {
   return (
     <>
       {sidebarData ? (
@@ -59,49 +52,26 @@ const SidebarFilter = () => {
   const specificPart = searchParams?.get('specific')
 
   return (
-    <>
-      {/* for small devices */}
-      <aside className='md:hidden flex-[0]'>
-        <Popover>
-          <PopoverTrigger asChild>
-            <Button variant='link'>
-              <CiMenuBurger className='cursor-pointer w-5 h-5' />
-            </Button>
-          </PopoverTrigger>
-          <PopoverContent className="w-80 flex flex-col gap-1 p-3">
-            {showFilterProduct && (
-              <SidebarContent
-                handleSearchFilter={handleSearchFilter}
-                sidebarData={sidebarData}
-                sidebarDataPending={sidebarDataPending}
-                specificPart={specificPart}
-              />
-            )}
-          </PopoverContent>
-        </Popover>
-      </aside>
-      {/* for devices greater than md */}
-      <aside className='hidden flex-1 md:block'>
-        <Card className='rounded-3xl bg-white shadow-lg'>
-          <CardHeader
-            className='text-nowrap border-b cursor-pointer py-2 font-medium text-primary-text transition-colors'
-            onClick={() => setShowFilterProduct(prev => !prev)}
-          >
-            Filter Products
-          </CardHeader>
-          {showFilterProduct && (
-            <CardContent className='flex flex-col gap-1 p-3'>
-              <SidebarContent
-                handleSearchFilter={handleSearchFilter}
-                sidebarData={sidebarData}
-                sidebarDataPending={sidebarDataPending}
-                specificPart={specificPart}
-              />
-            </CardContent>
-          )}
-        </Card>
-      </aside>
-    </>
+    <aside className='hidden flex-1 md:block'>
+      <Card className='rounded-3xl bg-white shadow-lg'>
+        <CardHeader
+          className='text-nowrap border-b cursor-pointer py-2 font-medium text-primary-text transition-colors'
+          onClick={() => setShowFilterProduct(prev => !prev)}
+        >
+          Filter Products
+        </CardHeader>
+        {showFilterProduct && (
+          <CardContent className='flex flex-col gap-1 p-3'>
+            <SidebarContent
+              handleSearchFilter={handleSearchFilter}
+              sidebarData={sidebarData}
+              sidebarDataPending={sidebarDataPending}
+              specificPart={specificPart}
+            />
+          </CardContent>
+        )}
+      </Card>
+    </aside>
   )
 }
 
