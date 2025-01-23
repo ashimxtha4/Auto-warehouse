ea447651f10e9e1bb24d8e4a456a0668265a638f
JiwanChaudhary <jeewanchaudhary6@gmail.com>
2025-01-23 21:35:05 +0545

    chore: user orders

--- src/components/orders/user-orders.tsx
+++ src/components/orders/user-orders.tsx
@@ -46,14 +46,14 @@ const UserOrders = () => {
 
 
   return (
-    <section ref={ref} className='mx-auto max-w-4xl p-6'>
+    <section ref={ref} className='mx-auto p-6'>
       <SectionHeader>My Orders</SectionHeader>
       <div className='space-y-6'>
         {ordersList?.length ? (
           ordersList.map(order => (
             <Card
               key={order.id}
-              className='flex flex-col items-start space-y-4 p-4 shadow-lg md:flex-row md:space-x-4 md:space-y-0'
+              className='flex flex-col items-start space-y-4 p-4 shadow-lg rounded-3xl md:flex-row md:space-x-4 md:space-y-0'
             >
               {/* Order Image */}
               <Image
@@ -67,18 +67,33 @@ const UserOrders = () => {
               {/* Order Info */}
               <div className='flex-grow'>
                 <h2 className='text-xl font-semibold'>{order.product_name}</h2>
-                <p className='mt-2 text-lg font-bold text-gray-700'>
-                  Price: ${order.product_price}
+                <p className='mt-2 text-lg text-gray-700'>
+                  Price:{' '}
+                  <span className='font-bold'>
+                    ${order.product_price}
+                  </span>
                 </p>
-                <p className='mt-1 text-lg'>SKU: {order.product_sku}</p>
+                <p className='mt-1 text-lg text-gray-700'>
+                  SKU:{' '}
+                  <span className='font-bold'>
+                    {order.product_sku}
+                  </span>
+                </p>
+                <p className='mt-1 text-lg text-gray-700'>
+                  Quantity:{' '}
+                  <span className='font-bold'>
+                    {order.quantity}
+                  </span>
+                </p>
 
                 {/* Order Status */}
-                <p className='mt-4'>
-                  <span className='font-semibold'>Status:</span>{' '}
+                <p className='mt-4 text-lg text-gray-700'>
+                  Status:{' '}
                   <span
                     className={cn(
-                      'rounded-md px-2 py-1 text-white',
+                      'rounded-full font-bold text-xl tracking-wide px-4 py-2 text-white',
                       order.status.includes('Cancelled') && 'bg-red-500',
+                      order.status.includes('Returned') && 'bg-red-500',
                       order.status.includes('Ordered') && 'bg-primary-main',
                       order.status.includes('Dispatched') && 'bg-blue-700'
                     )}
